ec40fffde30a7cf94be8559b43cab14377ff2d2e
Myrrhe <myrrhe@gmail.com>
2022-08-08 11:11:45 +0200

    Feat: Initialized admin

--- cantiniere/src/app/app-routing.module.ts
+++ cantiniere/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
