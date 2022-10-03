89c874126f3251874af0d1c158ec79685e6a9b75
LePygargue <46059303+LePygargue@users.noreply.github.com>
2022-10-03 17:11:37 +0200

    yep

--- cantiniere/src/app/app.module.ts
+++ cantiniere/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
