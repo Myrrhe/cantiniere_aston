aa7c80d046d1365d114d2c48b463d0fe50049a9e
LePygargue <46059303+LePygargue@users.noreply.github.com>
2022-10-03 15:08:36 +0200

    corrections

--- cantiniere/src/app/app-routing.module.ts
+++ cantiniere/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [
-  { path: 'admin', component: AdminComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cantiniere/src/app/app.module.ts
+++ cantiniere/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent,
   ],
   imports: [
     BrowserModule,
