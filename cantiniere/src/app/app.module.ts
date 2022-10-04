e2f6b57238d218add1071ef4d5fbcaf0a4829585
OmniaGomaa88 <minna1172018@gmail.com>
2022-10-04 11:56:32 +0200

    header/encour

--- cantiniere/src/app/app.module.ts
+++ cantiniere/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
