d470315dd10db9ddf19600b9f62ed82f6b2e357b
Myrrhe <myrrhe@gmail.com>
2022-10-05 14:18:48 +0200

    Feat: Added image, routing.

--- cantiniere/src/app/app-routing.module.ts
+++ cantiniere/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PageLoginComponent } from './page-login/page-login.component';
+
+const routes: Routes = [
+  { path: 'login', component: PageLoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
