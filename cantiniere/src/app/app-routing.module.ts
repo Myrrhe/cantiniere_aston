af582ab3cb11e06f83d87a581ac49a62baec1cb3
Myrrhe <myrrhe@gmail.com>
2022-10-07 09:47:02 +0200

    Feat: Added a creation account page WITHOUT validators.

--- cantiniere/src/app/app-routing.module.ts
+++ cantiniere/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './features/login/components/login/login.component';
+import { SigninComponent } from './features/signin/components/signin/signin.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signin', component: SigninComponent },
 ];
 
 @NgModule({
