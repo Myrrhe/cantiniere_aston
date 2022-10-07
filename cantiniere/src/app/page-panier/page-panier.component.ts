de61d9c718eff3cde6bd2d9c6b288b6c8132741c
OmniaGomaa88 <minna1172018@gmail.com>
2022-10-07 15:15:24 +0200

    affichag dynamique/totalFunction

--- cantiniere/src/app/page-panier/page-panier.component.ts
+++ cantiniere/src/app/page-panier/page-panier.component.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-panier.component.css']
 })
 export class PagePanierComponent implements OnInit {
+isAuth= false /* variable verfier si l'utilisature est connectée ou pas*/
+commands=[{
+jour:"lundi",
+plat:"plat1Name",
+qty:1,
+prix:23
+},
+{
+  jour:"judi",
+  plat:"plat2Name",
+  qty:2,
+  prix:15
+  },
+  {
+    jour:"mercurdi",
+    plat:"plat3Name",
+    qty:1,
+    prix:10
+    },
+]
+ total = this.commands.reduce((accumulator, obj) => {
+  return accumulator + obj.prix;
+}, 0);
 
   constructor() { }
 
