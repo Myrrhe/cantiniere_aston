2e8c29f2b56a5f0be53719f54731ef6f200eee13
LePygargue <46059303+LePygargue@users.noreply.github.com>
2022-12-01 20:50:29 +0100

    Récup du token par moi même.

--- cantiniere/src/app/features/auth/components/login/login.component.ts
+++ cantiniere/src/app/features/auth/components/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 //
 // import { };
 //
 import { AuthService } from '../../services/auth.service';
+// Interfaces
 import { Credential } from 'src/app/interfaces/credential';
+import { Token } from 'src/app/interfaces/token';
 
 
 @Component({
@@ -16,32 +18,34 @@ import { Credential } from 'src/app/interfaces/credential';
 })
 export class LoginComponent implements OnInit {
 
-  messageclass = ''
-  message = ''
-  Customerid: any;
-  editdata: any;
-  responsedata: any;
-
-  constructor(private http: HttpClient, private service: AuthService,private route:Router) {
-    localStorage.clear();
-  }
+  // messageclass = ''
+  // message = ''
+  // Customerid: any;
+  // editdata: any;
+  // responsedata: any;
 
   form: Credential = {
     email: '',
     password: ''
   };
 
+  constructor(private authServer: AuthService, private http: HttpClient, private service: AuthService,private route:Router) {
+    localStorage.clear();
+  }
+
   ngOnInit(): void {
   //   if(this.form.valid){
   // }
-}
+  }
 
   onSubmit(): void{
     console.log(this.form)
-    this.http.post('http://localhost:8080/stone.lunchtime/login',this.form).subscribe(
-      data => {
-        console.log(data)
-        // this.tokenService.saveToken(data.access_token)
+
+    this.authServer.login(this.form).subscribe(
+      resp => {
+        console.log(resp.headers.get('Authorization'))
+        // localStorage.setItem('token', data)
+        
       },
       err => console.log(err)
     )
--- cantiniere/src/app/features/auth/services/auth.service.ts
+++ cantiniere/src/app/features/auth/services/auth.service.ts
@@ -1,21 +1,24 @@
+// Lib
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-//
+// env
+import { environment } from 'src/app/environments/environment';
+// Interfaces
 import { Token } from 'src/app/interfaces/token';
 import { Credential } from 'src/app/interfaces/credential';
-import { environment } from 'src/app/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
 
   private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
-
-  login(credentials: Credential): Observable<Token>{
-    return this.http.post<Token>(this.apiUrl, credentials)
+ 
+  login(credentials: Credential): Observable<HttpResponse<Token>> {
+    return this.http.post<Token>(`${this.apiUrl}/login`, credentials, {observe: "response"})    
   }
 }
