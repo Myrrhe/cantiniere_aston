7203239ae44c1d49f1e583a21d909ef3c717928c
LePygargue <46059303+LePygargue@users.noreply.github.com>
2022-12-01 13:54:06 +0100

    avancement de l'auth

--- cantiniere/src/app/features/login/components/login/login.component.ts
+++ cantiniere/src/app/features/login/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  messageclass = ''
+  message = ''
+  Customerid: any;
+  editdata: any;
+  responsedata: any;
+
+  constructor(private service: AuthService,private route:Router) {
+    localStorage.clear();
+  }
+  Login = new FormGroup({
+    username: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required)
+  });
 
   ngOnInit(): void {
-  }
+    if(this.Login.valid){
 
+    }
+  }
 }
