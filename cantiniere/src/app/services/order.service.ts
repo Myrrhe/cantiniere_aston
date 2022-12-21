84e4be31de7266c94497152dc9eecacae4ef5268
Myrrhe <myrrhe@gmail.com>
2022-12-21 17:24:47 +0100

    feat: Added services.

--- cantiniere/src/app/services/order.service.ts
+++ cantiniere/src/app/services/order.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
+import { Order } from '../interfaces/order';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +24,24 @@ export class OrderService {
       constraintId: constraintId,
       quantity: quantity
     }, this.httpOptions);
+  }
+
+  // PATCH : /order/update/{orderId}
+  update(userId: number, constraintId: number, quantity: any[], orderId: number): Observable<any> {
+    return this.httpClient.patch(`${this.urlOrder}/update/${orderId}`, {
+      userId: userId,
+      constraintId: constraintId,
+      quantity: quantity
+    }, this.httpOptions);
+  }
+
+  // PATCH : /order/deliverandpay/{orderId}/{constraintId}
+  deliverandpay(orderId: number, constraintId: number): Observable<any> {
+    return this.httpClient.patch(`${this.urlOrder}/deliverandpay/${orderId}/${constraintId}`, {}, this.httpOptions);
+  }
+
+  // PATCH : /order/cancel/{orderId}/
+  cancel(orderId: number): Observable<any> {
+    return this.httpClient.patch(`${this.urlOrder}/cancel/${orderId}`, {}, this.httpOptions);
   }
 }
