1631d9030d89f8ffce4f78345da5d187e890302d
LePygargue <46059303+LePygargue@users.noreply.github.com>
2022-12-23 22:33:37 +0100

    early html/css admin

---
+++ cantiniere/src/app/services/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/app/environments/environment';
+import { User } from 'src/app/interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) 
+  {}
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/user/findall`);
+  }
+
+  getUser(id: string | null): Observable<User>{
+    return this.http.get<User>(`${this.apiUrl}/user/find/${id}`)
+  }
+
+
+  // getUser(): Observable<User> {
+  //   return this.http.get<User>(`${this.apiUrl}/user/find/1`);
+  // }
+
+  // createUser(user: User): Observable<User> {
+  //   return this.http.put<User>(`${this.apiUrl}/user/register`, user);
+  // }
+
+  // deleteUser(id: number): Observable<User> {
+  //   return this.http.delete<User>(`${this.apiUrl}/user/delete/${id}`);
+  // }
+}
