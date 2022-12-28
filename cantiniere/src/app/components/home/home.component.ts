67fb6cf62f506280babc10954086c64a086ad692
Myrrhe <myrrhe@gmail.com>
2022-12-28 13:24:36 +0100

    feature: Added comments.

--- cantiniere/src/app/components/home/home.component.ts
+++ cantiniere/src/app/components/home/home.component.ts
@@ -27,15 +27,19 @@ export class HomeComponent implements OnInit {
     });
 
     this.menuService.findAll().subscribe((data: any) => {
+      // We get the manus available for this week
       const dataFiltered: Menu[] = this.dateService.filterAvailableForThisWeek(data);
       for (const menu of dataFiltered) {
         const categories: number[] = [];
+        // We get the differents categories presents in each menus
         for (const meal of menu.meals) {
           if (!categories.some((category: number): boolean => meal.category === category)) {
             categories.push(meal.category);
           }
         }
+        // We sort the categories
         categories.sort((category1: number, category2: number): number => category1 - category2);
+        // We put each menus (and it's categories), in the right day
         for (let i = 1; i <= 7; i++) {
           if (this.dateService.isAvailableForDayOfThisWeek(menu, i)) {
             this.menuAvailableForThisWeek[i].push([menu, categories]);
